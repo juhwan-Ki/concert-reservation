24095b987181724338a169bb70afdf0272e815d6
juhwan <kjhw990218@gmail.com>
2025-08-20 21:01:45 +0900

    feat: init commit

---
+++ src/test/java/com/gomdol/concert/ServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.gomdol.concert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
