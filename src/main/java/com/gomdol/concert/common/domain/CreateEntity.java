d96e27e8938ffcdfad03b07aef71dd28297a5435
juhwan <kjhw990218@gmail.com>
2025-09-18 02:00:58 +0900

    feat: 공통 모듈 추가 - BaseEntity 및 공통 에러 클래스 추가

--- src/main/java/com/gomdol/concert/common/domain/CreateEntity.java
+++ src/main/java/com/gomdol/concert/common/domain/CreateEntity.java
@@ -1,9 +1,10 @@
 package com.gomdol.concert.common.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -11,8 +12,8 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseEntity extends CreateEntity{
-    @LastModifiedDate
-    @Column(nullable = false, name = "updated_at")
-    private LocalDateTime updatedAt;
+public abstract class CreateEntity {
+    @CreatedDate
+    @Column(nullable = false, name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
 }
